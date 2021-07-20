1aea1b44960e921e2cc7b7ffb371a17492d38178
Raghav Gupta <E19CSE258@bennett.edu.in>
2021-07-21 02:45:47 +0530

    Category Section Added

--- lib/Screens/HomeScreens/Home.dart
+++ lib/Screens/HomeScreens/Home.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:kawaii/Components/Home%20Screen/HomeComponents.dart';
 import 'package:kawaii/constants.dart';
+
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -16,12 +17,20 @@ class Home extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text("Welcome",style: TextStyle(
-              fontSize: 30,
-              fontFamily: 'subFont'
-            ),),
+            Text(
+              "Welcome",
+              style: TextStyle(fontSize: 30, fontFamily: 'subFont'),
+            ),
             BestSellerCard(),
-            PopularDeals()
+            SizedBox(
+              height: 15,
+            ),
+            PopularDeals(),
+            SizedBox(
+              height: 15,
+            ),
+            ShopByCategory(),
+            SizedBox(height: 100,)
           ],
         ),
       ),
@@ -30,36 +39,98 @@ class Home extends StatelessWidget {
 
   Column PopularDeals() {
     return Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("Popular Deals",style: ksmallFontStylewithStyle,),
-                  Text("See all",style: TextStyle(color: Colors.grey),),
-                ],
-              ),
-              SizedBox(height: 10,),
-              StreamBuilder<QuerySnapshot>(
-                stream: FirebaseFirestore.instance.collection('Items').snapshots(),
-                builder: (context,snapshot){
-                  var data = snapshot.data!.docs;
-                  return SizedBox(
-                    height: MediaQuery.of(context).size.height*0.35,
-                    child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: data.length,
-                      itemBuilder: (context,index){
-                        return ItemCard(data: data[index],);
-                      },
-                    ),
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "Popular Deals",
+              style: ksmallFontStylewithStyle,
+            ),
+            Text(
+              "See all",
+              style: TextStyle(color: Colors.grey),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance.collection('Items').snapshots(),
+          builder: (context, snapshot) {
+            var data = snapshot.data!.docs;
+            return SizedBox(
+              height: MediaQuery.of(context).size.height * 0.35,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: data.length,
+                itemBuilder: (context, index) {
+                  return ItemCard(
+                    data: data[index],
                   );
                 },
-              )
-            ],
-          );
+              ),
+            );
+          },
+        )
+      ],
+    );
+  }
+
+  Column ShopByCategory() {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "Shop By Category",
+              style: ksmallFontStylewithStyle,
+            ),
+            Text(
+              "See all",
+              style: TextStyle(color: Colors.grey),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance.collection('Category').snapshots(),
+          builder: (context, snapshot) {
+            var data = snapshot.data!.docs;
+            return SizedBox(
+              height: MediaQuery.of(context).size.height * 0.15,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: data.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    height: MediaQuery.of(context).size.height*0.1,
+                    width: MediaQuery.of(context).size.width*0.3,
+                    child: Column(
+                      children: [
+                        Container(
+                          height: MediaQuery.of(context).size.height*0.1,
+                          width: MediaQuery.of(context).size.width*0.3,
+                          child: FloatingActionButton(
+                            backgroundColor: Colors.white,
+                            onPressed: () => {},
+                            child: Image.network(data[index]['ImageUrl']),
+                          ),
+                        ),
+                        Text(data[index]['Name'],style: LoginFontStyle.copyWith(fontSize: 20),)
+                      ],
+                    )
+                  );
+                },
+              ),
+            );
+          },
+        )
+      ],
+    );
   }
 }
-
-
-
-
