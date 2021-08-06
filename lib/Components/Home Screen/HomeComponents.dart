2041918e1e8101c41b11568a9ab953f8ee2de0f8
Raghav Gupta <E19CSE258@bennett.edu.in>
2021-08-06 18:33:36 +0530

    Google Signin added

--- lib/Components/Home Screen/HomeComponents.dart
+++ lib/Components/Home Screen/HomeComponents.dart
@@ -14,51 +14,73 @@ class BestSellerCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Container(
-        margin: EdgeInsets.all(10),
-        decoration: cardDecoration.copyWith(gradient: LinearGradient(colors: [kcolor1, kcolor2]),),
-        height: MediaQuery.of(context).size.height * 0.25,
-        width: MediaQuery.of(context).size.width * 0.95,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width * 0.4,
-              padding: const EdgeInsets.all(15.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                    height: MediaQuery.of(context).size.height * 0.05,
-                    width: MediaQuery.of(context).size.width * 0.3,
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10)),
-                    child: Center(
-                      child: Text(
-                        "Best Seller",
-                        style: TextStyle(
-                            color: ksecColor, fontWeight: FontWeight.bold),
-                      ),
+      child: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('Admin').snapshots(),
+        builder: (context,snapshot){
+          return SizedBox(
+            height: MediaQuery.of(context).size.height * 0.35,
+            child: ListView.builder(
+                itemCount: snapshot.data!.docs.length,
+                physics: NeverScrollableScrollPhysics(),
+                itemBuilder: (context,index){
+              var data = snapshot.data!.docs[index];
+              if(data.id=='BestSeller')
+                return GestureDetector(
+                  onTap: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context)=>ProductCard(id: data['pid'],img: data['img'], name: data['Name'],price: data['Price'],)));
+                  },
+                  child: Container(
+                    margin: EdgeInsets.all(10),
+                    decoration: cardDecoration.copyWith(gradient: LinearGradient(colors: [kcolor1, kcolor2]),),
+                    height: MediaQuery.of(context).size.height * 0.25,
+                    width: MediaQuery.of(context).size.width * 0.95,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.4,
+                          padding: const EdgeInsets.all(15.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Container(
+                                height: MediaQuery.of(context).size.height * 0.05,
+                                width: MediaQuery.of(context).size.width * 0.3,
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(10)),
+                                child: Center(
+                                  child: Text(
+                                    "Best Seller",
+                                    style: TextStyle(
+                                        color: ksecColor, fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                              ),
+                              Text(
+                                data['Name'],
+                                style: ksmallFontStyle,
+                              ),
+                              Text(
+                                data['Price'],
+                                style: ksmallFontStyle,
+                              )
+                            ],
+                          ),
+                        ),
+                        Image.network(data['img'])
+                      ],
                     ),
                   ),
-                  Text(
-                    "Ethnic Women Pins",
-                    style: ksmallFontStyle,
-                  ),
-                  Text(
-                    "200 \$",
-                    style: ksmallFontStyle,
-                  )
-                ],
-              ),
-            ),
-            Image.network(
-                'https://firebasestorage.googleapis.com/v0/b/kawaii-510c4.appspot.com/o/2-removebg-preview.png?alt=media&token=a9c16524-d131-4437-bbbd-44a3ef4a9d0c')
-          ],
-        ),
+                );
+              else{
+                return Container();
+              }
+            }),
+          );
+        },
       ),
     );
   }
--- lib/Screens/Auth/Login.dart
+++ lib/Screens/Auth/Login.dart
@@ -1,7 +1,10 @@
 import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:kawaii/Screens/HomeScreens/HomeScreen.dart';
 
 import '../../constants.dart';
@@ -21,62 +24,80 @@ class _LoginState extends State<Login> {
   final TextEditingController _phoneNumberController = TextEditingController();
   final TextEditingController _smsController = TextEditingController();
   late String _verificationId;
-
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+  Future<User?> _signIn() async {
+    await Firebase.initializeApp();
+    final GoogleSignInAccount? googleSignInAccount =
+    await googleSignIn.signIn();
+    final GoogleSignInAuthentication googleSignInAuthentication =
+    await googleSignInAccount!.authentication;
+    final AuthCredential credential = GoogleAuthProvider.credential(
+      idToken: googleSignInAuthentication.idToken,
+      accessToken: googleSignInAuthentication.accessToken,
+    );
+    User? user = (await auth.signInWithCredential(credential)).user;
+    // if (user != null) {
+    //   name = user.displayName;
+    //   email = user.email;
+    // }
+    return user;
+  }
   void showSnackbar(String message) {
     final snackBar = SnackBar(
       content: Text(message),
     );
     ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
-  void verifyPhoneNumber() async {
-    PhoneVerificationCompleted verificationCompleted =
-        (PhoneAuthCredential phoneAuthCredential) async {
-      await _auth.signInWithCredential(phoneAuthCredential);
-      showSnackbar(
-          "Phone number automatically verified and user signed in: ${_auth.currentUser!.uid}");
-    };
-    PhoneVerificationFailed verificationFailed =
-        (FirebaseAuthException authException) {
-      showSnackbar(
-          'Phone number verification failed. Code: ${authException.code}. Message: ${authException.message}');
-    };
-    PhoneCodeSent codeSent =
-        (String verificationId, [int? forceResendingToken]) {
-      showSnackbar('Please check your phone for the verification code.');
-      _verificationId = verificationId;
-    };
-    PhoneCodeAutoRetrievalTimeout codeAutoRetrievalTimeout =
-        (String verificationId) {
-      showSnackbar("verification code Sent");
-      _verificationId = verificationId;
-    };
-    try {
-      await _auth.verifyPhoneNumber(
-          phoneNumber: "+91 " + _phoneNumberController.text,
-          timeout: const Duration(seconds: 5),
-          verificationCompleted: verificationCompleted,
-          verificationFailed: verificationFailed,
-          codeSent: codeSent,
-          codeAutoRetrievalTimeout: codeAutoRetrievalTimeout);
-    } catch (e) {
-      showSnackbar("Failed to Verify Phone Number: $e");
-    }
-  }
-  void signInWithPhoneNumber() async {
-    try {
-      final AuthCredential credential = PhoneAuthProvider.credential(
-        verificationId: _verificationId,
-        smsCode: _smsController.text,
-      );
-
-      final User? user = (await _auth.signInWithCredential(credential)).user;
-      FirebaseFirestore.instance.collection('cust').doc(FirebaseAuth.instance.currentUser!.uid).set({});
-      showSnackbar("Successfully signed");
-      Navigator.pushReplacementNamed(context, HomeScreen.id);
-    } catch (e) {
-      showSnackbar("Failed to sign in: " + e.toString());
-    }
-  }
+  // void verifyPhoneNumber() async {
+  //   PhoneVerificationCompleted verificationCompleted =
+  //       (PhoneAuthCredential phoneAuthCredential) async {
+  //     await _auth.signInWithCredential(phoneAuthCredential);
+  //     showSnackbar(
+  //         "Phone number automatically verified and user signed in: ${_auth.currentUser!.uid}");
+  //   };
+  //   PhoneVerificationFailed verificationFailed =
+  //       (FirebaseAuthException authException) {
+  //     showSnackbar(
+  //         'Phone number verification failed. Code: ${authException.code}. Message: ${authException.message}');
+  //   };
+  //   PhoneCodeSent codeSent =
+  //       (String verificationId, [int? forceResendingToken]) {
+  //     showSnackbar('Please check your phone for the verification code.');
+  //     _verificationId = verificationId;
+  //   };
+  //   PhoneCodeAutoRetrievalTimeout codeAutoRetrievalTimeout =
+  //       (String verificationId) {
+  //     showSnackbar("verification code Sent");
+  //     _verificationId = verificationId;
+  //   };
+  //   try {
+  //     await _auth.verifyPhoneNumber(
+  //         phoneNumber: "+91 " + _phoneNumberController.text,
+  //         timeout: const Duration(seconds: 5),
+  //         verificationCompleted: verificationCompleted,
+  //         verificationFailed: verificationFailed,
+  //         codeSent: codeSent,
+  //         codeAutoRetrievalTimeout: codeAutoRetrievalTimeout);
+  //   } catch (e) {
+  //     showSnackbar("Failed to Verify Phone Number: $e");
+  //   }
+  // }
+  // void signInWithPhoneNumber() async {
+  //   try {
+  //     final AuthCredential credential = PhoneAuthProvider.credential(
+  //       verificationId: _verificationId,
+  //       smsCode: _smsController.text,
+  //     );
+  //
+  //     final User? user = (await _auth.signInWithCredential(credential)).user;
+  //     FirebaseFirestore.instance.collection('cust').doc(FirebaseAuth.instance.currentUser!.uid).set({});
+  //     showSnackbar("Successfully signed");
+  //     Navigator.pushReplacementNamed(context, HomeScreen.id);
+  //   } catch (e) {
+  //     showSnackbar("Failed to sign in: " + e.toString());
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -90,10 +111,37 @@ class _LoginState extends State<Login> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  LoginText(),
-                  OptAndPhoneNumber(),
-                  OTP_LoginButton(),
-                  WrongPhoneNumber(),
+                  Center(child: LoginText()),
+                  GestureDetector(
+                    onTap: ()async{
+                      _signIn().whenComplete(() {
+                        Navigator.pushReplacementNamed(context,HomeScreen.id);
+                      }).catchError((onError) {
+                        print(onError);
+                      });
+                    },
+                    child: Container(
+                      height: 70,
+                      decoration: BoxDecoration(
+                          color: LoginColor, borderRadius: BorderRadius.circular(20)),
+                      child: Center(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(FontAwesomeIcons.google,color: Colors.white,),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              "Login",
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        )
+                      ),
+                    ),
+                  ),
                   SizedBox(
                     height: MediaQuery.of(context).size.height * 0.2,
                   )
@@ -115,9 +163,27 @@ class _LoginState extends State<Login> {
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 LoginText(),
-                OptAndPhoneNumber(),
-                OTP_LoginButton(),
-                WrongPhoneNumber(),
+                GestureDetector(
+                  onTap: ()async{
+                    _signIn().whenComplete(() {
+                      Navigator.pushReplacementNamed(context,HomeScreen.id);
+                    }).catchError((onError) {
+                      print(onError);
+                    });
+                  },
+                  child: Container(
+                    height: 50,
+                    decoration: BoxDecoration(
+                        color: LoginColor, borderRadius: BorderRadius.circular(20)),
+                    child: Center(
+                      child: Text(
+                        "Login",
+                        style: TextStyle(
+                            color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
                 SizedBox(
                   height: MediaQuery.of(context).size.height * 0.2,
                 )
@@ -127,91 +193,8 @@ class _LoginState extends State<Login> {
     );
   }
 
-  GestureDetector WrongPhoneNumber() {
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          isOtpSent = false;
-        });
-      },
-      child: Text(
-        "Wrong number ? enter again",
-        style: TextStyle(fontSize: 18, color: LoginColor),
-      ),
-    );
-  }
 
-  GestureDetector OTP_LoginButton() {
-    return GestureDetector(
-      onTap: () async {
-        if(isOtpSent == false){
-          setState(() {
-            verifyPhoneNumber();
-            isOtpSent = true;
-          });
-        }
-        else{
-          signInWithPhoneNumber();
-          //Navigator.pushReplacementNamed(context, MainScreen.id);
-        }
-      },
-      child: Container(
-        height: 50,
-        decoration: BoxDecoration(
-            color: LoginColor, borderRadius: BorderRadius.circular(20)),
-        child: Center(
-          child: Text(
-            (isOtpSent) ? "Login" : "Get OTP",
-            style: TextStyle(
-                color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-        ),
-      ),
-    );
-  }
 
-  Center OptAndPhoneNumber() {
-    return Center(
-      child: Column(
-        children: [
-          TextField(
-            keyboardType: TextInputType.number,
-            onChanged: (value) {
-              setState(() {
-                phoneNUmber = value;
-              });
-            },
-            controller: _phoneNumberController,
-            decoration: InputDecoration(
-                labelText: "Phone Number",
-                fillColor: ksecColor,
-                labelStyle: TextStyle(fontSize: 25),
-                hintText: "Enter your phone number",
-                border: OutlineInputBorder()),
-          ), //Following is the mobile number field
-          SizedBox(
-            height: 25,
-          ),
-          Visibility(
-            child: TextField(
-              onChanged: (value){
-
-              },
-              controller: _smsController,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  labelText: "OTP",
-                  labelStyle: TextStyle(fontSize: 25),
-                  fillColor: ksecColor,
-                  hintText: "Enter your OTP",
-                  border: OutlineInputBorder()),
-            ),
-            visible: isOtpSent,
-          ) //Following is the OTP enter screen
-        ],
-      ),
-    );
-  }
 }
 
 class LoginText extends StatelessWidget {
